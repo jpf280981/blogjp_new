020ef61c58fd9ba93df1c73b1130a42003467bfe
jpf280981 <jpf280981@yahoo.com.br>
2023-04-21 09:38:57 -0300

    adicionado formulario cadastro/novo texto

--- src/app/(blog)/noticia/page.js
+++ src/app/(blog)/noticia/page.js
@@ -1,15 +1,63 @@
-'use server'
 
-export async function generateMetadata({ params }) {
-    return {
-      title: 'Cadastro de notícia'
-    };
+
+'use client'
+import Link from 'next/link';
+import { useState } from 'react';
+
+function blogjp() {
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [text, setText] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log(title, subtitle, text);
   }
+  
+  return (
+    
+    <div>
+      <h1>Cadastro de notícia</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Título da notícia:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          required
+        /><br /><br />
 
-export default async function Page(){
-    return(
-        <>
-            <h2>Cadastro de notícia</h2>
-        </>
-    )
+        <label htmlFor="subtitle">Subtitulo da notícia:</label>
+        <input
+          type="text"
+          id="subtitle"
+          name="subtitle"
+          value={subtitle}
+          onChange={(event) => setSubtitle(event.target.value)}
+          required
+        /><br /><br />
+
+        <label htmlFor="text">Texto:</label><br />
+        <textarea
+          id="text"
+          name="text"
+          rows="10"
+          cols="50"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          required
+        ></textarea><br /><br />
+        
+
+        <button type="submit">Salvar</button>
+        <Link href="/">Página principal</Link>
+      </form>
+    </div>
+    
+  );
 }
+
+export default blogjp;
